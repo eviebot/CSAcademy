43429ed6dbc4bd54d79501b5399b0a2ff2dd5e6a
Saraansh Chopra <saraansh.chopra@gmail.com>
2017-06-15 13:02:14 +0530

    1-31 June 2017

---
+++ Round 33/numbers_tour.cpp
@@ -0,0 +1,212 @@
+/*                                      .....'',;;::cccllllllllllllcccc:::;;,,,''...'',,'..
+                            ..';cldkO00KXNNNNXXXKK000OOkkkkkxxxxxddoooddddddxxxxkkkkOO0XXKx:.
+                      .':ok0KXXXNXK0kxolc:;;,,,,,,,,,,,;;,,,''''''',,''..              .'lOXKd'
+                 .,lx00Oxl:,'............''''''...................    ...,;;'.             .oKXd.
+              .ckKKkc'...'',:::;,'.........'',;;::::;,'..........'',;;;,'.. .';;'.           'kNKc.
+           .:kXXk:.    ..       ..................          .............,:c:'...;:'.         .dNNx.
+          :0NKd,          .....''',,,,''..               ',...........',,,'',,::,...,,.        .dNNx.
+         .xXd.         .:;'..         ..,'             .;,.               ...,,'';;'. ...       .oNNo
+         .0K.         .;.              ;'              ';                      .'...'.           .oXX:
+        .oNO.         .                 ,.              .     ..',::ccc:;,..     ..                lXX:
+       .dNX:               ......       ;.                'cxOKK0OXWWWWWWWNX0kc.                    :KXd.
+     .l0N0;             ;d0KKKKKXK0ko:...              .l0X0xc,...lXWWWWWWWWKO0Kx'                   ,ONKo.
+   .lKNKl...'......'. .dXWN0kkk0NWWWWWN0o.            :KN0;.  .,cokXWWNNNNWNKkxONK: .,:c:.      .';;;;:lk0XXx;
+  :KN0l';ll:'.         .,:lodxxkO00KXNWWWX000k.       oXNx;:okKX0kdl:::;'',;coxkkd, ...'. ...'''.......',:lxKO:.
+ oNNk,;c,'',.                      ...;xNNOc,.         ,d0X0xc,.     .dOd,           ..;dOKXK00000Ox:.   ..''dKO,
+'KW0,:,.,:..,oxkkkdl;'.                'KK'              ..           .dXX0o:'....,:oOXNN0d;.'. ..,lOKd.   .. ;KXl.
+;XNd,;  ;. l00kxoooxKXKx:..ld:         ;KK'                             .:dkO000000Okxl;.   c0;      :KK;   .  ;XXc
+'XXdc.  :. ..    '' 'kNNNKKKk,      .,dKNO.                                   ....       .'c0NO'      :X0.  ,.  xN0.
+.kNOc'  ,.      .00. ..''...      .l0X0d;.             'dOkxo;...                    .;okKXK0KNXx;.   .0X:  ,.  lNX'
+ ,KKdl  .c,    .dNK,            .;xXWKc.                .;:coOXO,,'.......       .,lx0XXOo;...oNWNXKk:.'KX;  '   dNX.
+  :XXkc'....  .dNWXl        .';l0NXNKl.          ,lxkkkxo' .cK0.          ..;lx0XNX0xc.     ,0Nx'.','.kXo  .,  ,KNx.
+   cXXd,,;:, .oXWNNKo'    .'..  .'.'dKk;        .cooollox;.xXXl     ..,cdOKXXX00NXc.      'oKWK'     ;k:  .l. ,0Nk.
+    cXNx.  . ,KWX0NNNXOl'.           .o0Ooldk;            .:c;.':lxOKKK0xo:,.. ;XX:   .,lOXWWXd.      . .':,.lKXd.
+     lXNo    cXWWWXooNWNXKko;'..       .lk0x;       ...,:ldk0KXNNOo:,..       ,OWNOxO0KXXNWNO,        ....'l0Xk,
+     .dNK.   oNWWNo.cXK;;oOXNNXK0kxdolllllooooddxk00KKKK0kdoc:c0No        .'ckXWWWNXkc,;kNKl.          .,kXXk,
+      'KXc  .dNWWX;.xNk.  .kNO::lodxkOXWN0OkxdlcxNKl,..        oN0'..,:ox0XNWWNNWXo.  ,ONO'           .o0Xk;
+      .ONo    oNWWN0xXWK, .oNKc       .ONx.      ;X0.          .:XNKKNNWWWWNKkl;kNk. .cKXo.           .ON0;
+      .xNd   cNWWWWWWWWKOkKNXxl:,'...;0Xo'.....'lXK;...',:lxk0KNWWWWNNKOd:..   lXKclON0:            .xNk.
+      .dXd   ;XWWWWWWWWWWWWWWWWWWNNNNNWWNNNNNNNNNWWNNNNNNWWWWWNXKNNk;..        .dNWWXd.             cXO.
+      .xXo   .ONWNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNK0ko:'..OXo          'l0NXx,              :KK,
+      .OXc    :XNk0NWXKNWWWWWWWWWWWWWWWWWWWWWNNNX00NNx:'..       lXKc.     'lONN0l.              .oXK:
+      .KX;    .dNKoON0;lXNkcld0NXo::cd0NNO:;,,'.. .0Xc            lXXo..'l0NNKd,.              .c0Nk,
+      :XK.     .xNX0NKc.cXXl  ;KXl    .dN0.       .0No            .xNXOKNXOo,.               .l0Xk;.
+     .dXk.      .lKWN0d::OWK;  lXXc    .OX:       .ONx.     . .,cdk0XNXOd;.   .'''....;c:'..;xKXx,
+     .0No         .:dOKNNNWNKOxkXWXo:,,;ONk;,,,,,;c0NXOxxkO0XXNXKOdc,.  ..;::,...;lol;..:xKXOl.
+     ,XX:             ..';cldxkOO0KKKXXXXXXXXXXKKKKK00Okxdol:;'..   .';::,..':llc,..'lkKXkc.
+     :NX'    .     ''            ..................             .,;:;,',;ccc;'..'lkKX0d;.
+     lNK.   .;      ,lc,.         ................        ..,,;;;;;;:::,....,lkKX0d:.
+    .oN0.    .'.      .;ccc;,'....              ....'',;;;;;;;;;;'..   .;oOXX0d:.
+    .dN0.      .;;,..       ....                ..''''''''....     .:dOKKko;.
+     lNK'         ..,;::;;,'.........................           .;d0X0kc'.
+     .xXO'                                                 .;oOK0x:.
+      .cKKo.                                    .,:oxkkkxk0K0xc'.
+        .oKKkc,.                         .';cok0XNNNX0Oxoc,.
+          .;d0XX0kdlc:;,,,',,,;;:clodkO0KK0Okdl:,'..
+              .,coxO0KXXXXXXXKK0OOxdoc:,..
+  ░░░░░░░░░░░░▄▄▄▄░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄░░░░
+  ░░░█░░░░▄▀█▀▀▄░░▀▀▀▄░░░░▐█░░░░░░░░░▄▀█▀▀▄░░░▀█▄░
+  ░░█░░░░▀░▐▌░░▐▌░░░░░▀░░░▐█░░░░░░░░▀░▐▌░░▐▌░░░░█▀
+  ░▐▌░░░░░░░▀▄▄▀░░░░░░░░░░▐█▄▄░░░░░░░░░▀▄▄▀░░░░░▐▌
+  ░█░░░░░░░░░░░░░░░░░░░░░░░░░▀█░░░░░░░░░░░░░░░░░░█
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█   - PROPRIETARY CODE OF EVIEBOT
+  ▐█░░░░░░░░░░░░░░░░░░░░░░░░░░█▌░░░░░░░░░░░░░░░░░█
+  ░█░░░░░░░░░░░░░░░░░░░░█▄░░░▄█░░░░░░░░░░░░░░░░░░█
+  ░▐▌░░░░░░░░░░░░░░░░░░░░▀███▀░░░░░░░░░░░░░░░░░░▐▌
+  ░░█░░░░░░░░░░░░░░░░░▀▄░░░░░░░░░░▄▀░░░░░░░░░░░░█░
+  ░░░█░░░░░░░░░░░░░░░░░░▀▄▄▄▄▄▄▄▀▀░░░░░░░░░░░░░█░
+  ▒▒▒░░░░░░░░░░▄▐░░░░
+  ▒░░░░░░▄▄▄░░▄██▄░░░
+  ░░░░░░▐▀█▀▌░░░░▀█▄░
+  ░░░░░░▐█▄█▌░░░░░░▀█▄
+  ░░░░░░░▀▄▀░░░▄▄▄▄▄▀▀
+  ░░░░░▄▄▄██▀▀▀▀░░░░░
+  ░░░░█▀▄▄▄█░▀▀░░░░░░
+  ░░░░▌░▄▄▄▐▌▀▀▀░░░░░
+  ░▄░▐░░░▄▄░█░▀▀░░░░░   DON'T MESS WITH MR. SKELTAL
+  ░▀█▌░░░▄░▀█▀░▀░░░░░
+  ░░░░░░░░▄▄▐▌▄▄░░░░░
+  ░░░░░░░░▀███▀█░▄░░░
+  ░░░░░░░▐▌▀▄▀▄▀▐▄░░░
+  ░░░░░░░▐▀░░░░░░▐▌░░
+  ░░░░░░░█░░░░░░░░█░░
+  ░░░░░░▐▌░░░░░░░░░█░  */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define MP make_pair
+
+typedef long long int lli;
+ofstream outfile;
+ifstream infile;
+
+bool comp(pair <int, int> a, pair <int, int> b)
+{
+    if( a.fi > b.fi)
+        return true;
+    else if(a.fi < b.fi)
+        return false;
+
+    else
+    {
+        if( a.se < b.se)
+            return true;
+        return false;
+    }
+}
+int main()
+{
+    infile.open("input.txt");
+    outfile.open("output.txt");
+
+    //ios::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+
+    int n, i, j, t, k;
+    int storage[100][100];
+    vector <pair <int, int>> result;
+    vector <vector <pair <bool, int>>> visited;
+
+    vector <pair <bool, int>> temp(10001);
+
+
+    fill(temp.begin(), temp.end(), MP(0, 0));
+
+
+    cin >> n;
+    for(i =0 ; i < n; ++i)
+    {
+        visited.pb(temp);
+        result.pb(MP(0, i));
+    }
+    for(i = 0;i < n; ++i)
+    {
+        for(j = 0; j < n; ++j)
+        {
+            cin >> t;
+            visited[i][t].fi = 1;
+            visited[i][t].se = j;
+            storage[i][j] = t;
+        }
+    }
+    int index_cmax[2] , index_cmin[2] , cmax = 0, cmin = 100000, sum[2];
+    for(i = 0 ; i < n; ++i)
+    {
+        for(j = i+1; j < n; ++j)
+        {
+            index_cmax[0] = -1;
+            index_cmin[0] = -1;
+            cmax = 0;cmin = 100000;
+            for(k = 0; k < n; ++k)
+            {
+                if( visited[i][storage[j][k]].fi)
+                {
+                    if(storage[j][k] > cmax)
+                    {
+                        cmax = storage[j][k];
+                        index_cmax[0] = visited[i][storage[j][k]].se;
+                        index_cmax[1] = k;
+                    }
+                    if(storage[j][k] < cmin)
+                    {
+                        cmin = storage[j][k];
+                        index_cmin[1] = k;
+                        index_cmin[0] = visited[i][storage[j][k]].se;
+                    }
+                }
+            }
+            //cout << index_cmin[0] << " "<< index_cmax[0] <<"\n";
+            //cout << index_cmin[1] << " "<< index_cmax[1] << "\n";
+            if( index_cmax[0] == -1 || index_cmax[1] == index_cmin[1])
+            {
+                result[i].fi++;
+                ++result[j].fi;
+            }
+            else
+            {
+                if(index_cmin[0] < index_cmax[0])
+                {
+                    sum[0] = index_cmin[0] + n-1-index_cmax[0];
+                }
+                else
+                {
+                    sum[0] = index_cmax[0] + n-1-index_cmin[0];
+                }
+                if(index_cmin[1] < index_cmax[1])
+                {
+                    sum[1] = index_cmin[1] + n-1-index_cmax[1];
+                }
+                else
+                {
+                    sum[1] = index_cmax[1] + n-1-index_cmin[1];
+                }
+
+                if( sum[0] > sum[1])
+                {
+                    result[i].fi += 2;
+                }
+                else if( sum[1] > sum[0])
+                {
+                    result[j].fi += 2;
+                }
+                else
+                {
+                    result[i].fi++;
+                    result[j].fi++;
+                }
+            }
+            //cout << i << ' '<< result[i].fi<<'\n';
+            //cout << j <<' '<< result[j].fi << "\n\n";
+        }
+    }
+
+    sort(result.begin(), result.end(), comp);
+    for(i = 0; i < n; ++i)
+        cout << result[i].se+1 << '\n';
+}
